68991fe7e5c2d131218f82dae189b1cba050bf84
André Glatzl <andre@alpinesoft.ch>
2021-01-11 16:46:11 +0000

    Added corona warning alert component

---
+++ frontend/src/components/CoronaWarning.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class CoronaWarning extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dismiss: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({ dismiss: true });
+    }
+
+    render() {
+        return (
+            <div>
+                { this.state.dismiss == false && (
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <div class="container">
+                            <h6>Coronavirus (COVID-19) Support</h6>
+                            <p>
+                                Bitte überprüfen Sie die Reisebeschränkungen. Reisen sind möglicherweise nur für bestimmte Zwecke gestattet, insbesondere touristische Reisen sind möglicherweise nicht gestattet.
+                                </p>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close" onClick={this.handleClick}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+export default CoronaWarning;
