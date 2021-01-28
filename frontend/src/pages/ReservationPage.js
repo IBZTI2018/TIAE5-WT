d5fad578c970ca7c975cdf5c5c09df5590554691
JenniferSusann <Jennifermentner@hotmail.com>
2021-01-28 20:14:39 +0100

    Reservation overview
    Just a Page to show Reservation
     nothing in it at the moment

---
+++ frontend/src/pages/ReservationPage.js
@@ -0,0 +1,12 @@
+import Reservation from '../components/Reservation';
+
+function ReservationPage() {
+    return (
+        <div>
+            <h1>Buchungen: </h1>
+            <Reservation />
+        </div>
+    );
+}
+
+export default ReservationPage;
