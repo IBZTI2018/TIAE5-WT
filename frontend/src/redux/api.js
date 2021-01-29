4934780f79c645f456f95f9dd802480f8230b4d0
Sven Gehring <cbrxde@gmail.com>
2021-01-29 17:00:14 +0100

    adjust REST API base path

--- frontend/src/redux/api.js
+++ frontend/src/redux/api.js
@@ -1,6 +1,6 @@
 import APIClient from "@holidayextras/jsonapi-client";
 
-export default new APIClient("http://localhost:8080/api", {
+export default new APIClient("http://localhost:8080/api/v1", {
   header: {
     authToken: "2ad1d6f7-e1d0-480d-86b2-dfad8af4a5b3",
   },
