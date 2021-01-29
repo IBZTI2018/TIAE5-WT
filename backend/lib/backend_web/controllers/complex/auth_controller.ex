c62185daba9e8fff17bb92a4d6647d11061f715e
Sven Gehring <cbrxde@gmail.com>
2021-01-29 17:19:15 +0100

    fix error codes on backend for unauthorized

--- backend/lib/backend_web/controllers/complex/auth_controller.ex
+++ backend/lib/backend_web/controllers/complex/auth_controller.ex
@@ -50,14 +50,14 @@ defmodule BackendWeb.AuthController do
          {:ok, password} <- Map.fetch(args, "password") do
       sign_in_user(conn, usermail, password)
     else
-      :error -> :bad_request
+      :error -> {:error, :bad_request}
     end
   end
 
   defp sign_in_user(conn, usermail, password) do
     case Database.get_user_by_mail(usermail) do
       nil ->
-        :unauthorized
+        {:error, :unauthorized}
 
       user ->
         if Pbkdf2.verify_pass(password, user.password) do
@@ -67,7 +67,7 @@ defmodule BackendWeb.AuthController do
           |> put_status(200)
           |> json(%{data: %{token: token}})
         else
-          :unauthorized
+          {:error, :unauthorized}
         end
     end
   end
