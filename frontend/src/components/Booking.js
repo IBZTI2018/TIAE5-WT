6d6a4adfb3ee536e47d7e6cba63a71f34f437904
JenniferSusann <Jennifermentner@hotmail.com>
2021-01-30 11:22:59 +0100

    offers not offer

--- frontend/src/components/Booking.js
+++ frontend/src/components/Booking.js
@@ -10,7 +10,7 @@ class Booking extends Component {
     }
 
     handleOffer(event) {
-        this.props.history.push('/offer');
+        this.props.history.push('/offers');
         event.preventDefault();
         return false;
     }
