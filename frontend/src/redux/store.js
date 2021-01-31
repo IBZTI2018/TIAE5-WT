3836a36e510624745ff7f3448354ac9b0c419488
Sven Gehring <cbrxde@gmail.com>
2021-01-31 11:38:29 +0100

    add support for redux store chrome plugin

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,20 @@
 
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import thunk from "redux-thunk";
 import search from "./search";
 import offers from "./offers";
 
-const store = createStore(combineReducers({ search, offers }), applyMiddleware(thunk));
+const reducers = {
+  search,
+  offers,
+}
+
+const store = createStore(
+  combineReducers(reducers),
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
