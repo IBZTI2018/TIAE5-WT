34e855c27e0e9f9e27e93206189e7c70feebba2d
Sven Gehring <cbrxde@gmail.com>
2021-01-31 12:18:47 +0100

    properly pass auth token to backend from redux store

--- frontend/src/redux/api.js
+++ frontend/src/redux/api.js
@@ -1,7 +1,9 @@
-import APIClient from "@holidayextras/jsonapi-client";
+import JSONAPIClient from "@holidayextras/jsonapi-client";
+import store from './store';
+import { getAuthToken } from './auth/selectors';
 
-export default new APIClient("http://localhost:8080/api/v1", {
+export default new JSONAPIClient("http://localhost:8080/api/v1", {
   header: {
-    authToken: "2ad1d6f7-e1d0-480d-86b2-dfad8af4a5b3",
-  },
+    'Authorization': `Bearer ${getAuthToken(store.getState())}`
+  }
 });
