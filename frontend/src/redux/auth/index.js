2a980267a69e1f249d2384cf2e3e41770f72774f
Sven Gehring <cbrxde@gmail.com>
2021-01-31 12:03:05 +0100

    add pragmatic local store persistence to auth stoer

--- frontend/src/redux/auth/index.js
+++ frontend/src/redux/auth/index.js
@@ -1,14 +1,34 @@
 import types from './types';
 
+const LOCAL_STORAGE_TOKEN = 'tiae5AuthToken';
+const LOCAL_STORAGE_EMAIL = 'tiae5UserMail';
+
 const initialState = {
   isLoggedIn: false,
   authToken: null,
   userEmail: null
 }
 
+const dumpStateOnLocalStorage = (token, email) => {
+  window.localStorage.setItem(LOCAL_STORAGE_TOKEN, token);
+  window.localStorage.setItem(LOCAL_STORAGE_EMAIL, email);
+}
+
+const loadStateFromLocalStorage = () => {
+  const token = window.localStorage.getItem(LOCAL_STORAGE_TOKEN);
+  const email = window.localStorage.getItem(LOCAL_STORAGE_EMAIL);
+
+  if (email && token) return { isLoggedIn: true, authToken: token, userEmail: email };
+  return initialState;
+}
+
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case types.AUTHENTICATE_USER:
+      
+      // Pragmatic persistence to local storage
+      dumpStateOnLocalStorage(payload.token, payload.email);
+
       return {
         ...state,
         isLoggedIn: true,
@@ -24,6 +44,11 @@ export default (state = initialState, { type, payload }) => {
         authToken: null,
         userEmail: null
       }
+      break;
+
+    case "@@INIT":
+      // Pragmatic persistence from local storage
+      return loadStateFromLocalStorage();
       break;
 
     default:
