bac0c29bfa68ce20d116edb3366d0b3c3f7f365d
Sven Gehring <cbrxde@gmail.com>
2021-02-01 14:42:25 +0100

    re-add redux dev tools as optional improvement
    So that it no longer causes a type error for people that do not have it installed...

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -5,17 +5,29 @@ import search from "./search";
 import offers from "./offers";
 import auth from "./auth";
 
+let middlewares = [];
 const reducers = {
   search,
   offers,
   auth
 }
 
-const store = createStore(
-  combineReducers(reducers),
-  compose(
+// Very elaborate check to make sure our store also works without the Redux Chrome addon
+//
+// The documentation at github.com/zalmoxisus/redux-devtools-extension suggests a nice
+// one-liner to add this, however, that causes a type error for every user without the
+// dev tools installed (Issue: github.com/zalmoxisus/redux-devtools-extension/issues/413)
+//
+// This makes sure that the devtools extension middleware is only used if available!
+if (typeof(window.__REDUX_DEVTOOLS_EXTENSION__) !== 'undefined') {
+  middlewares = compose(
     applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-);
+} else {
+  middlewares = applyMiddleware(thunk)
+}
+
+const store = createStore(combineReducers(reducers), middlewares);
 
 export default store;
