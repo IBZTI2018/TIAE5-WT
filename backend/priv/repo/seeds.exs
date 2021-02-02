8badeb162155fd4e26c3583ba85e1e871fd16757
André Glatzl <andre@alpinesoft.ch>
2021-02-02 18:09:23 +0000

    Changed hotel image seed

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -101,7 +101,7 @@ for i <- 1..10 do
   hotel =
     Backend.Repo.insert!(%Backend.Schema.Hotel{
       hotelname: Faker.Company.name(),
-      image: "https://placedog.net/640/480?random",
+      image: "https://placedog.net/640/480?id=#{i}",
       address: random_address.()
     })
 
