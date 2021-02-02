4f433c1674ab41ae33e1244853a1fb262332922a
André Glatzl <andre@alpinesoft.ch>
2021-02-02 19:05:35 +0000

    Fix bad programming from André...

--- frontend/src/redux/offers/actions.js
+++ frontend/src/redux/offers/actions.js
@@ -1,14 +1,14 @@
 import types from "./types";
 import api from "../api";
 
-const _fetchOffers = (payload) => ({ type: types.FETCH_OFFERS, payload });
 export const fetchOffers = () => async (dispatch) => {
   return api.find("offers", {
       include: "hotelroom.hotel.address.street.city.country"
     },
     (err, resources) => {
         resources = resources || []
-        dispatch(_fetchOffers(resources.map((resource) => resource.toJSONTree())))
+        const payload = resources.map((resource) => resource.toJSONTree())
+        dispatch({ type: types.FETCH_OFFERS, payload })
     }
   );
 };
