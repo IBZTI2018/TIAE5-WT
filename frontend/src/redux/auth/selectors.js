18580c2dc95c66b60e7c1e281bcdcd01e60afc92
Dardan <dardan.ahmeti@student.ibz.ch>
2021-02-06 14:42:50 +0100

    added isManager function to auth selectors

--- frontend/src/redux/auth/selectors.js
+++ frontend/src/redux/auth/selectors.js
@@ -5,3 +5,9 @@ export const authState = store => store.auth;
 export const isLoggedIn = store => authState(store).isLoggedIn;
 export const getAuthToken = store => authState(store).authToken;
 export const getUserData = store => authState(store).user;
+export const isManager = store => isLoggedIn(store) && authState(store).user.isManager;
+
+/***
+ * 
+ * 
+ */
