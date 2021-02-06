71ccc845d5fa190185e1ed83246e56bdf3da6774
Sven Gehring <cbrxde@gmail.com>
2021-02-06 18:10:02 +0100

    fix null not being ignored by toJSONTree agnostic helper

--- frontend/src/redux/json.js
+++ frontend/src/redux/json.js
@@ -1,7 +1,8 @@
 // Empty data agnostic toJSONTree
 export const toJSONTree = (input) => {
+  if (input === null) return {};
   if (input === undefined) return {};
   if (typeof(input) !== 'object') return {};
-  
+
   return input.toJSONTree();
 }
