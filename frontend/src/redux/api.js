f2e3b4035dc11a9565a57a3873382a6e6b336ae8
Sven Gehring <cbrxde@gmail.com>
2021-02-06 22:40:54 +0100

    make api host dynamic

--- frontend/src/redux/api.js
+++ frontend/src/redux/api.js
@@ -2,7 +2,7 @@ import JSONAPIClient from "@holidayextras/jsonapi-client";
 import store from './store';
 import { getAuthToken } from './auth/selectors';
 
-export default new JSONAPIClient("http://localhost:8080/api/v1", {
+export default new JSONAPIClient(`${window.location.origin}/api/v1`, {
   header: {
     'Authorization': `Bearer ${getAuthToken(store.getState())}`
   }
