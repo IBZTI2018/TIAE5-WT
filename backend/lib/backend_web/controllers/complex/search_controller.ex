2abab8222167433c3e32e878c69e47a0e5ceec41
André Glatzl <andre@alpinesoft.ch>
2021-02-07 19:42:23 +0000

    Added more params for searching offers

--- backend/lib/backend_web/controllers/complex/search_controller.ex
+++ backend/lib/backend_web/controllers/complex/search_controller.ex
@@ -7,7 +7,7 @@ defmodule BackendWeb.SearchController do
 
   action_fallback(BackendWeb.FallbackController)
 
-  @invalid_search_chars ~r/[^0-9A-zÄÖÜäöüÉÈÀéèà]/
+  @invalid_search_chars ~r/[^\-0-9A-zÄÖÜäöüÉÈÀéèà]/
   @search_query """
   SELECT o.id FROM offers o
     INNER JOIN hotelrooms hr
@@ -20,13 +20,32 @@ defmodule BackendWeb.SearchController do
       ON a.street_id = s.id
     INNER JOIN cities c
       ON s.city_id = c.id
-    WHERE h.hotelname LIKE '%$1%'
-      OR c.cityname LIKE '%$1%';
+    WHERE 
+      (h.hotelname LIKE '%$1%' OR c.cityname LIKE '%$1%')
+      AND
+      (o.validitystart >= '$2' AND o.validityend <= '$3')
+      AND
+      (hr.persons = $4)  
+    ;
   """
 
-  def search_hotels(conn, %{"query" => query}) do
+  today = DateTime.utc_now
+  validitystart = [today.year, today.month, today.day] |> Enum.map(&to_string/1) |> Enum.map(&String.pad_leading(&1, 2, "0")) |> Enum.join("-")
+  validityend = today |> Date.add(7)
+  validityend = [validityend.year, validityend.month, validityend.day] |> Enum.map(&to_string/1) |> Enum.map(&String.pad_leading(&1, 2, "0")) |> Enum.join("-")
+
+  @optional_params %{"validitystart" => validitystart, "validityend" => validityend, "persons" => 1}
+  def search_hotels(conn, %{"query" => query} = params) do
+    params = Map.merge(@optional_params, params)
+    persons = params["persons"]
     with secure_string <- String.replace(query, @invalid_search_chars, "%"),
+         secure_validitystart <- String.replace(params["validitystart"], @invalid_search_chars, "%"),
+         secure_validityend <- String.replace(params["validityend"], @invalid_search_chars, "%"),
+         secure_persons <- String.replace("#{persons}", @invalid_search_chars, "%"),
          secure_query <- String.replace(@search_query, "$1", secure_string),
+         secure_query <- String.replace(secure_query, "$2", secure_validitystart),
+         secure_query <- String.replace(secure_query, "$3", secure_validityend),
+         secure_query <- String.replace(secure_query, "$4", secure_persons),
          {:ok, %MyXQL.Result{rows: r}} <- Ecto.Adapters.SQL.query(Backend.Repo, secure_query),
          offer_ids <- Enum.map(r, fn [x] -> x end) do
       data = Database.get_offers_list(offer_ids)
