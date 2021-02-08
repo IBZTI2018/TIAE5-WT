95417375033b89425a99baff0c148edea17c8d66
Sven Gehring <cbrxde@gmail.com>
2021-02-08 09:36:01 +0100

    minor search controller cleanup

--- backend/lib/backend_web/controllers/complex/search_controller.ex
+++ backend/lib/backend_web/controllers/complex/search_controller.ex
@@ -20,32 +20,35 @@ defmodule BackendWeb.SearchController do
       ON a.street_id = s.id
     INNER JOIN cities c
       ON s.city_id = c.id
-    WHERE 
+    WHERE
       (h.hotelname LIKE '%$1%' OR c.cityname LIKE '%$1%')
       AND
       (o.validitystart >= '$2' AND o.validityend <= '$3')
       AND
-      (hr.persons = $4)  
+      (hr.persons = $4)
     ;
   """
 
-  today = DateTime.utc_now
-  validitystart = [today.year, today.month, today.day] |> Enum.map(&to_string/1) |> Enum.map(&String.pad_leading(&1, 2, "0")) |> Enum.join("-")
-  validityend = today |> Date.add(365)
-  validityend = [validityend.year, validityend.month, validityend.day] |> Enum.map(&to_string/1) |> Enum.map(&String.pad_leading(&1, 2, "0")) |> Enum.join("-")
+  def search_hotels(conn, %{"query" => query} = request_params) do
+    validitystart = DateTime.utc_now() |> Date.to_string()
+    validityend = DateTime.utc_now() |> Date.add(365) |> Date.to_string()
 
-  @optional_params %{"validitystart" => validitystart, "validityend" => validityend, "persons" => 1}
-  def search_hotels(conn, %{"query" => query} = params) do
-    params = Map.merge(@optional_params, params)
-    persons = params["persons"]
-    with secure_string <- String.replace(query, @invalid_search_chars, "%"),
-         secure_validitystart <- String.replace(params["validitystart"], @invalid_search_chars, "%"),
-         secure_validityend <- String.replace(params["validityend"], @invalid_search_chars, "%"),
-         secure_persons <- String.replace("#{persons}", @invalid_search_chars, "%"),
-         secure_query <- String.replace(@search_query, "$1", secure_string),
-         secure_query <- String.replace(secure_query, "$2", secure_validitystart),
-         secure_query <- String.replace(secure_query, "$3", secure_validityend),
-         secure_query <- String.replace(secure_query, "$4", secure_persons),
+    default_params = %{
+      "validitystart" => validitystart,
+      "validityend" => validityend,
+      "persons" => 1
+    }
+
+    params = Map.merge(default_params, request_params)
+
+    with secure_string <- escape_string(query),
+         secure_validitystart <- escape_string(params["validitystart"]),
+         secure_validityend <- escape_string(params["validityend"]),
+         secure_persons <- escape_string("#{params["persons"]}"),
+         secure_query <- insert_param(@search_query, "$1", secure_string),
+         secure_query <- insert_param(secure_query, "$2", secure_validitystart),
+         secure_query <- insert_param(secure_query, "$3", secure_validityend),
+         secure_query <- insert_param(secure_query, "$4", secure_persons),
          {:ok, %MyXQL.Result{rows: r}} <- Ecto.Adapters.SQL.query(Backend.Repo, secure_query),
          offer_ids <- Enum.map(r, fn [x] -> x end) do
       data = Database.get_offers_list(offer_ids)
@@ -54,4 +57,7 @@ defmodule BackendWeb.SearchController do
       _ -> {:error, :bad_request}
     end
   end
+
+  defp escape_string(str), do: String.replace(str, @invalid_search_chars, "%")
+  defp insert_param(query, param, value), do: String.replace(query, param, value)
 end
