6aa3f8385185f4de7c4fbe36631e8a42dec027ba
André Glatzl <andre@alpinesoft.ch>
2021-02-11 12:11:56 +0000

    Fix null bug on redux url state

--- frontend/src/redux/offers/selectors.js
+++ frontend/src/redux/offers/selectors.js
@@ -1,14 +1,26 @@
 import moment from "moment";
 
 // States
-export const offersState = store => store.offers;
+export const offersState = (store) => store.offers;
 
 // Selectors
-export const getOffers = store => offersState(store).offers;
-export const isFetching = store => offersState(store).isFetching;
-export const getSearchQuery = store => offersState(store).searchQuery;
-export const getStartDate = store => offersState(store).startDate;
-export const getEndDate = store => offersState(store).endDate;
-export const getStartDateFormatted = store => moment(offersState(store).startDate).format('YYYY-MM-DD');
-export const getEndDateFormatted = store => moment(offersState(store).endDate).format('YYYY-MM-DD');
-export const getGuestCounter = store => offersState(store).guestCounter;
+export const getOffers = (store) => offersState(store).offers;
+export const isFetching = (store) => offersState(store).isFetching;
+export const getSearchQuery = (store) => offersState(store).searchQuery;
+export const getStartDate = (store) => offersState(store).startDate;
+export const getEndDate = (store) => offersState(store).endDate;
+export const getStartDateFormatted = (store) => {
+  let startDate = offersState(store).startDate;
+  if (startDate) {
+    return moment(startDate).format("YYYY-MM-DD");
+  }
+  return null;
+};
+export const getEndDateFormatted = (store) => {
+  let endDate = offersState(store).endDate;
+  if (endDate) {
+    return moment(endDate).format("YYYY-MM-DD");
+  }
+  return null;
+};
+export const getGuestCounter = (store) => offersState(store).guestCounter;
