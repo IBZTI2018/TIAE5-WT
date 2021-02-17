47357668008cd773a3c708dd9c3ce7bf4ef859b7
Sven Gehring <cbrxde@gmail.com>
2021-02-17 15:23:17 +0100

    permission scope offers

--- backend/lib/backend_web/controllers/offer_controller.ex
+++ backend/lib/backend_web/controllers/offer_controller.ex
@@ -1,8 +1,10 @@
 defmodule BackendWeb.OfferController do
   use BackendWeb, :controller
 
+  alias Backend.Repo
   alias Backend.Database
   alias Backend.Schema.Offer
+  alias Backend.Schema.Hotelroom
 
   alias BackendWeb.OfferView
 
@@ -25,21 +27,22 @@ defmodule BackendWeb.OfferController do
   end
 
   def create(conn, args) do
-    with {:ok, data} <- Database.generic_create(Offer, args) do
+    hotelroom_id = args["data"]["attributes"]["hotelroom_id"]
+    hotelroom = Database.generic_item(Hotelroom, hotelroom_id)
+
+    with :ok <- can_manage_hotel(conn, hotelroom.hotel_id),
+         {:ok, data} <- Database.generic_create(Offer, args) do
       conn
       |> put_status(:created)
       |> render("show.json", %{data: data})
     end
   end
 
-  def update(conn, args) do
-    with {:ok, data} <- Database.generic_update(Offer, args["id"], args) do
-      render(conn, "show.json", %{data: data})
-    end
-  end
-
   def delete(conn, args) do
-    with {:ok, _} <- Database.generic_delete(Offer, args["id"]) do
+    offer = Database.generic_item(Offer, args["id"]) |> Repo.preload(:hotelroom)
+
+    with :ok <- can_manage_hotel(conn, offer.hotelroom.hotel_id),
+         {:ok, _} <- Database.generic_delete(Offer, args["id"]) do
       send_resp(conn, 200, "")
     end
   end
